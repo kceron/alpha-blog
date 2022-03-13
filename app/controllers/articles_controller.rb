93a938df306cd517e82a88a0f1cb0fc04a7a83ad
Karem Ceron <karemiceron@gmail.com>
2022-03-13 14:08:11 -0400

    saving newly created articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,17 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+
+  end
+
+  def create
+    # render plain: params[:article] # to see plain article params 
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect # too see @article details
+    @article.save
+    # redirect_to article_path(@article) # redirect to show. shortcut below
+    redirect_to @article
+  end 
+
 end
