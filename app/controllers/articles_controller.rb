5662e775061b33f875e3e6374263c573dacbfd8c
Karem Ceron <karemiceron@gmail.com>
2022-03-14 17:04:23 -0400

    dry refactor/used partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
 
   def show
     # byebug
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -14,12 +14,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
     # render plain: params[:article] # to see plain article params 
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     # render plain: @article.inspect # too see @article details
     if @article.save
       # redirect_to article_path(@article) # redirect to show. shortcut below
@@ -31,8 +30,7 @@ class ArticlesController < ApplicationController
   end 
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
@@ -41,9 +39,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
+  private 
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
