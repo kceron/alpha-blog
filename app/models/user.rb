8fd3cfc68538b3d7576b276e885e70042eb6d95c
Karem Ceron <karemiceron@gmail.com>
2022-03-23 18:09:34 -0400

    add users table and model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  # validates :title, presence: true, length: { minimum: 4, maximum: 80 }
+  # validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+
+end
