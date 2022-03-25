c5b29584c3dd6148131c4d9e1f78b4ed89490e02
Karem Ceron <karemiceron@gmail.com>
2022-03-24 21:46:51 -0400

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  # validates :title, presence: true, length: { minimum: 4, maximum: 80 }
-  # validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,   
+            uniqueness: { case_sensitive: false }, length: { maximum: 100 },
+            format: { with: VALID_EMAIL_REGEX }
 
 end
